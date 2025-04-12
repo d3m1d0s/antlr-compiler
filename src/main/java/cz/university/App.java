4bc67d4c9dad60a847f97e7d94a3751cdd064b1e
Demid Ostiakov <demid.ostiakov@gmail.com>
2025-04-12 18:31:34 +0200

    add VerboseListener.java

--- src/main/java/cz/university/App.java
+++ src/main/java/cz/university/App.java
@@ -1,13 +1,27 @@
 package cz.university;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+import java.io.IOException;
+
+public class App {
+    private static final String EXT = "lang";
+    private static final String DIR = "src/test/resources/";
+    public static void main(String[] args) throws IOException {
+        String file = args.length == 0 ? "test." + EXT : args[0];
+        System.out.println("START: " + file);
+
+        CharStream input = CharStreams.fromFileName(DIR + file);
+        LanguageLexer lexer = new LanguageLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        LanguageParser parser = new LanguageParser(tokens);
+
+        parser.removeErrorListeners();
+        parser.addErrorListener(new VerboseListener());
+
+        ParseTree tree = parser.program(); // start rule
+        System.out.println(tree.toStringTree(parser));
+        System.out.println("FINISH: " + file);
     }
 }
---
+++ src/main/java/cz/university/VerboseListener.java
@@ -0,0 +1,21 @@
+package cz.university;
+
+import org.antlr.v4.runtime.*;
+
+import java.util.Collections;
+import java.util.List;
+
+public class VerboseListener extends BaseErrorListener {
+    @Override
+    public void syntaxError(Recognizer<?, ?> recognizer,
+                            Object offendingSymbol,
+                            int line, int charPositionInLine,
+                            String msg,
+                            RecognitionException e) {
+        List<String> stack = ((Parser) recognizer).getRuleInvocationStack();
+        Collections.reverse(stack);
+        System.err.println("rule stack: " + stack);
+        System.err.println("line " + line + ":" + charPositionInLine + " at " + offendingSymbol + ": " + msg);
+        System.exit(1);  // stop execution at the first syntax error.
+    }
+}
